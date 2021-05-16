077cc05ef7ebd187ad426d35bb1fb1bb838f13a1
briankim1512@gmail.com <briankim1512@gmail.com>
2021-05-16 19:56:27 +0900

    Add intro frame

---
+++ app/src/static/js/index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Intro Frame Javascript
+    let html = document.querySelector('html')
+    let introButton = document.querySelector('.intro-btn')
+    
+    introButton.addEventListener('click', function () {
+        html.requestFullscreen()
+    })
+
+    // Landing Frame Javascript
+
+    // Logging Frame Javascript
+
+    // Playing Frame Javascript
+
+    // Sharing Frame Javascript
+})
