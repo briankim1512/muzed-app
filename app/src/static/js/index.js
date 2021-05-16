5c4b5804fe5c888189ad2419444f78e9ec2d43a6
briankim1512@gmail.com <briankim1512@gmail.com>
2021-05-17 00:55:11 +0900

    Add basic landing frame and functionality

--- app/src/static/js/index.js
+++ app/src/static/js/index.js
@@ -8,6 +8,26 @@ document.addEventListener('DOMContentLoaded', function () {
     })
 
     // Landing Frame Javascript
+    // !!! Dummy Data for testing  !!!
+    const entries = [
+        {date: 1, songName: "a", songArtist: "a"},
+        {date: 2, songName: "b", songArtist: "b"},
+        {date: 3, songName: "c", songArtist: "c"},
+        {date: 4, songName: "d", songArtist: "d"},
+        {date: 5, songName: "e", songArtist: "e"},
+        {date: 6, songName: "f", songArtist: "f"},
+        {date: 7, songName: "g", songArtist: "g"},
+        {date: 8, songName: "h", songArtist: "h"},
+        {date: 9, songName: "a", songArtist: "a"}
+    ]
+
+    let diary = new Vue({
+        el: '#landing-diary',
+        delimiters: ['[[',']]'],
+        data: {
+            entries: entries
+        }
+    })
 
     // Logging Frame Javascript
 
