99960f809d4960705dd625b7f896f3a6136d77d3
briankim1512@gmail.com <briankim1512@gmail.com>
2021-05-20 09:29:20 +0900

    Finish playing functionality

--- app/src/static/js/index.js
+++ app/src/static/js/index.js
@@ -34,6 +34,10 @@ async function searchNapster (searchTerm = '') {
     return trackInfo
 }
 
+function generateUID () {
+    return Date.now().toString(36) + Math.random().toString(36).substring(2)
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     // Intro Frame Javascript
     let html = document.querySelector('html')
@@ -54,22 +58,33 @@ document.addEventListener('DOMContentLoaded', function () {
     // !!! Dummy Data for testing  !!!
     let entries = [
         {
+            id: generateUID(),
             date: "2020-01-01",
             title: "Bohemian Rhapsody",
             artist: "Queen",
             album: "http://static.rhap.com/img/170x170/7/7/1/4/12294177_170x170.jpg",
             preview: "https://listen.hs.llnwd.net/g3/prvw/4/4/9/2/0/1448702944.mp3",
-            note: "Test",
-            memories: null
+            note: "The Bohemian Rhapsody, while widely known, has a special meaning for me as...",
+            image: "https://upload.wikimedia.org/wikipedia/commons/2/20/Bohemian_Rhapsody.jpg",
+            memories: {
+                people: {
+                    list: ['Freddie Mercury'],
+                    description: 'Short Description'
+                }
+            }
         },
         {
+            id: generateUID(),
             date: "2020-01-01",
             title: "I Want to Break Free",
             artist: "Queen",
             album: "http://static.rhap.com/img/170x170/0/6/1/4/24694160_170x170.jpg",
             preview: "https://listen.hs.llnwd.net/g3/prvw/2/8/5/7/5/2312157582.mp3",
-            note: "Test",
-            memories: null
+            note: "This song's lyrics is relatable to nearly everyone and anyone...",
+            image: "https://cdn10.picryl.com/photo/1991/06/08/fireworks-light-up-the-night-sky-behind-the-washington-monument-at-the-conclusion-6783a8-1600.jpg",
+            memories: {
+                mood: 'HAPPY'
+            }
         }
     ]
 
@@ -88,6 +103,33 @@ document.addEventListener('DOMContentLoaded', function () {
             list: []
         }
     })
+
+    let peopleMemory = new Vue({
+        el: '#people-memory-card',
+        delimiters: ['[[',']]'],
+        data: {
+            list: [],
+            description: ''
+        }
+    })
+
+    function updateEntryList () {
+        setTimeout(function() {
+            document.querySelectorAll('.diary-entry').forEach(element => {
+                let entryUID = element.childNodes[0].innerHTML
+                let entryIndex = diary.entries.findIndex(element => element.id == entryUID)
+                if (!('tagged' in diary.entries[entryIndex])) {
+                    element.addEventListener('click', function () {
+                        playEntry(diary.entries[entryIndex])
+                    })
+                    diary.entries[entryIndex]['tagged'] = true
+                }
+    
+            })
+        }, 100)
+    }
+
+    updateEntryList()
 
     let landingAdd = document.querySelector('.landing-add')
     let loggingFrame = document.querySelector('#logging-frame')
@@ -139,6 +181,7 @@ document.addEventListener('DOMContentLoaded', function () {
         previousFrame = null
         moodLog = null
         peopleList.list = []
+        loggingInfo = {}
         loggingFrame.style.opacity = 0
 
         setTimeout(function () {
@@ -225,10 +268,11 @@ document.addEventListener('DOMContentLoaded', function () {
         loggingInfo['date'] = new Date().toJSON().slice(0,10)
         loggingInfo['note'] = loggingStory.value
         loggingInfo['memories'] = null
+        loggingInfo['id'] = generateUID()
         loggingStory.value = ""
 
         diary.entries.push(loggingInfo)
-        loggingInfo = {}
+        updateEntryList()
 
         clearLogFrames()
     })
@@ -268,16 +312,130 @@ document.addEventListener('DOMContentLoaded', function () {
             description: peopleDetail.value
         }
         loggingInfo['memories']['mood'] = moodLog
+        loggingInfo['id'] = generateUID()
 
         diary.entries.push(loggingInfo)
-        loggingInfo = {}
-
-        console.log(diary.entries)
+        updateEntryList()
 
         clearLogFrames()
     })
 
     // Playing Frame Javascript
+    let playingFrame = document.querySelector('#playing-frame')
+    let playingExit = document.querySelector('#playing-exit')
+    let playingAlbum = document.querySelector('#playing-album')
+    let playingImage = document.querySelector('#playing-image')
+    let playingStart = document.querySelector('#playing-start')
+    let playingSubframe = document.querySelector('#playing-subframe')
+    let playingSubframeIndicator = document.querySelector('#playing-subframe-indicator')
+    let playingNotes = document.querySelector('#playing-notes')
+    let playingMemories = document.querySelector('#playing-memories')
+    let notesSubframe = document.querySelector('#note-subframe')
+    let memoriesSubframe = document.querySelector('#memories-subframe')
+    let notesSubframeText = document.querySelector('#note-subframe-text')
+    let moodMemoryImg = document.querySelector('#mood-memory-img')
+    let playingSubframeStatus = false
+    let playingAudio = new Audio()
+    let playingStatus = false
+
+    function playEntry (data) {
+        playingFrame.style.display = 'flex'
+        setTimeout(function() {
+            playingFrame.style.top = '0%'
+    
+            playingAlbum.style.backgroundImage = 'url(' + data.album + ')'
+            playingAudio.src = data.preview
+            notesSubframeText.innerHTML = data.note
+    
+            // Set Image
+            playingImage.style.backgroundImage = ''
+            if ('image' in data) {
+                playingImage.style.backgroundImage = 'url(' + data.image + ')'
+            }
+
+            // Set Memories
+            document.querySelector('#people-memory-card').style.display = 'none'
+            document.querySelector('#mood-memory-card').style.display = 'none'
+
+            peopleMemory.data = {
+                list: [],
+                description: ''
+            }
+            moodMemoryImg.src = ''
+
+            if (data.memories != null) {
+                if ('people' in data.memories) {
+                    peopleMemory.list = data.memories.people.list
+                    peopleMemory.description = data.memories.people.description
+                    document.querySelector('#people-memory-card').style.display = 'flex'
+                }
+                if ('mood' in data.memories) {
+                    let moodName = 'static/img/emoji/' + data.memories.mood.toLowerCase() + '.svg'
+                    moodMemoryImg.src = moodName
+                    document.querySelector('#mood-memory-card').style.display = 'flex'
+                }
+            }
+    
+            playingAudio.play()
+            playingStatus = true
+        }, 10)
+    }
+
+    function clearPlayingSubframes () {
+        notesSubframe.style.display = 'none'
+        memoriesSubframe.style.display = 'none'
+    }
+
+    playingExit.addEventListener('click', function () {
+        playingAudio.pause()
+        playingFrame.style.top = '100%'
+        playingSubframe.style.height = '12%'
+        playingSubframeStatus = false
+        clearPlayingSubframes()
+        setTimeout(function () {
+            playingFrame.style.display = 'none'
+        }, 400)
+    })
+
+    playingStart.addEventListener('click', function () {
+        if (playingStatus == true) {
+            playingAudio.pause()
+            playingStatus = false
+        } else {
+            playingAudio.play()
+            playingStatus = true
+        }
+    })
+
+    playingSubframeIndicator.addEventListener('click', function() {
+        if (playingSubframeStatus == false) {
+            playingSubframe.style.height = '85%'
+            playingSubframeStatus = true
+            notesSubframe.style.display = 'flex'
+        } else {
+            playingSubframe.style.height = '12%'
+            playingSubframeStatus = false
+            clearPlayingSubframes()
+        }
+    })
+
+    playingNotes.addEventListener('click', function() {
+        if (playingSubframeStatus == false) {
+            playingSubframe.style.height = '85%'
+            playingSubframeStatus = true
+        }
+        clearPlayingSubframes()
+        notesSubframe.style.display = 'flex'
+    })
+
+    playingMemories.addEventListener('click', function() {
+        if (playingSubframeStatus == false) {
+            playingSubframe.style.height = '85%'
+            playingSubframeStatus = true
+        }
+        clearPlayingSubframes()
+        memoriesSubframe.style.display = 'flex'
+    })
 
     // Sharing Frame Javascript
 })
